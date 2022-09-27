f56b7e9ff58bed91f0aae22c474e60159157a74e
Lauren Moghimi <moghimi@stanford.edu>
2022-09-27 09:54:40 -0700

    Update raman_plot.m

--- raman_plot.m
+++ raman_plot.m
@@ -9,31 +9,35 @@ folder_path = 'G:\My Drive\Lab - science work and meetings\Data Measurements\Ram
 
 figure
 %%input all files that you want plotted below
-%all_files = ["3-11-21 Te-EDA on Silica 0-200 60 s 10 p.xlsx";"bulk GeAsTe .5 p 105 s - from 3-23-21 Raman Summary.xlsx"]; %%
-all_files = ["100nm_01.txt";"100nm_02.txt";"100nm_03.txt";"100nm_04.txt"];
+all_files = ["5um_01.txt";"5um_02.txt";"5um_03.txt"]; %%
 hold on
 for m = 1:length(all_files) 
-    filename = all_files(m,1)
+    filename = all_files(m,1);
     full_path = fullfile(folder_path, filename);
     fid = fopen(full_path,'rt');
+    fprintf(['Loading ',filename,'...'])
     
     T = readtable(full_path,'ReadRowNames',false);
     x = T{:,1};
     y = T{:,2};
+    
     y = m*y; %scale vertically to create waterfall plot
-    fid = fclose(fid);
     %TF = islocalmax(y);
     %plot(x,y,x(TF),y(TF),'r*',LineWidth=2)
     plot(x,y,LineWidth=2)
+    
+    fid = fclose(fid);
+    fprintf('done!\n')
 end
 %% Adjusting Figure Layout
 xlabel('Raman Shift (cm^{-1})','FontSize',16)
-%xticks(50:25:200) %%
 ylabel('a.u.','FontSize',16)
 axis([min(x) max(x) 0 max(y)]) %%
+%xticks(50:25:200) %%
+
 %%label the series in the order that they were inputted above
-legend(all_files)
+legend(all_files) %%
 hold off
-title('100 nm Fe3O4','FontSize',16) %%
+title('5 um Fe3O4','FontSize',16) %%
 
 
