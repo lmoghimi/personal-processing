a46e043dd04d4259a93d286411ea3409633214cb
Lauren Moghimi <moghimi@stanford.edu>
2022-10-03 21:58:54 -0700

    Update raman_plot.m

--- raman_plot.m
+++ raman_plot.m
@@ -15,8 +15,10 @@ folder_path = 'G:\My Drive\Lab - science work and meetings\Data Measurements\Ram
 
 %%input all files that you want plotted below
 all_files = ["5um_01.txt";"5um_02.txt";"5um_03.txt"]; %%
-%load files
-PlottingVars = struct();
+%all_files = ["100nm_01.txt";"100nm_02.txt";"100nm_03.txt";"100nm_04.txt"];
+
+%% Load files
+RawData = struct();
 for m = 1:length(all_files) 
     filename = all_files(m,1);
     full_path = fullfile(folder_path, filename);
@@ -27,27 +29,41 @@ for m = 1:length(all_files)
     x = T{:,1};
     y = T{:,2};
     y_max = max(y); y_norm = y/y_max; %normalize
-    PlottingVars(m).x = x;
-    PlottingVars(m).y = y;
-    PlottingVars(m).ynorm = y_norm;
+    RawData(m).x = x;
+    RawData(m).y = y;
+    RawData(m).ynorm = y_norm;
+
     fid = fclose(fid);
     fprintf('done!\n')
 end
+
+%% Substrate division
+PlottingVars = struct();
+substrate = RawData(2).y;
+for m = 1:length(all_files) 
+    PlottingVars(m).substrate_div = RawData(m).y ./ substrate;
+    y_max = max(PlottingVars(m).substrate_div);
+    PlottingVars(m).ynorm = PlottingVars(m).substrate_div / y_max;
+end
+
 %% Plot
 figure
 hold on
 xlabel('Raman Shift (cm^{-1})','FontSize',16)
 ylabel('Normalized Counts (a.u.)','FontSize',16)
 title('5 um Fe3O4','FontSize',16) %%
+%
 for m = 1:length(all_files)
     %y = m*y; %scale vertically to create waterfall plot
-    plot(PlottingVars(m).x,PlottingVars(m).ynorm,LineWidth=2)
+    plot(RawData(m).x,PlottingVars(m).ynorm,LineWidth=2)
     %TF = islocalmax(y);
     %plot(x,y,x(TF),y(TF),'r*',LineWidth=2)
 end
-axis([min(PlottingVars(m).x) max(PlottingVars(m).x) 0 1]) %%
+%
+axis([min(RawData(m).x) max(RawData(m).x) 0 1]) %%
 %%label the series in the order that they were inputted above
-legend(all_files) %%
+%legend(all_files) %%
+legend(["5um_01 divided by 5um_02";"5um_02 divided by 5um_02";"5um_03 divided by 5um_02"]);
 hold off
 
 
