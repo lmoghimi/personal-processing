7b1dbab6105ac4aab48a3040fb203946d1351bf4
Lauren Moghimi <moghimi@stanford.edu>
2022-10-03 21:32:04 -0700

    Update raman_plot.m

--- raman_plot.m
+++ raman_plot.m
@@ -13,10 +13,10 @@ close all
 %% Opening the data files and plotting in the same figure
 folder_path = 'G:\My Drive\Lab - science work and meetings\Data Measurements\Raman\22-9-26 mFe3O4';
 
-figure
 %%input all files that you want plotted below
 all_files = ["5um_01.txt";"5um_02.txt";"5um_03.txt"]; %%
-hold on
+%load files
+PlottingVars = struct();
 for m = 1:length(all_files) 
     filename = all_files(m,1);
     full_path = fullfile(folder_path, filename);
@@ -26,26 +26,29 @@ for m = 1:length(all_files)
     T = readtable(full_path,'ReadRowNames',false);
     x = T{:,1};
     y = T{:,2};
-
-    y_max = max(y); y = y/y_max; %normalize
-    %y = m*y; %scale vertically to create waterfall plot
-    plot(x,y,LineWidth=2)
-    %TF = islocalmax(y);
-    %plot(x,y,x(TF),y(TF),'r*',LineWidth=2)
-    
-    
+    y_max = max(y); y_norm = y/y_max; %normalize
+    PlottingVars(m).x = x;
+    PlottingVars(m).y = y;
+    PlottingVars(m).ynorm = y_norm;
     fid = fclose(fid);
     fprintf('done!\n')
 end
-%% Adjusting Figure Layout
+%% Plot
+figure
+hold on
 xlabel('Raman Shift (cm^{-1})','FontSize',16)
 ylabel('Normalized Counts (a.u.)','FontSize',16)
-axis([min(x) max(x) 0 max(y)]) %%
-%xticks(50:25:200) %%
-
+title('5 um Fe3O4','FontSize',16) %%
+for m = 1:length(all_files)
+    %y = m*y; %scale vertically to create waterfall plot
+    plot(PlottingVars(m).x,PlottingVars(m).ynorm,LineWidth=2)
+    %TF = islocalmax(y);
+    %plot(x,y,x(TF),y(TF),'r*',LineWidth=2)
+end
+axis([min(PlottingVars(m).x) max(PlottingVars(m).x) 0 1]) %%
 %%label the series in the order that they were inputted above
 legend(all_files) %%
 hold off
-title('5 um Fe3O4','FontSize',16) %%
+
 
 
