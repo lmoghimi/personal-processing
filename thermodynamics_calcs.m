7f4c0b6e2b5b9bae2156374f0295bb72b910a6e1
Lauren Moghimi <moghimi@stanford.edu>
2023-06-05 16:51:19 -0700

    approximate calculations, assuming H and S are T invariant

--- thermodynamics_calcs.m
+++ thermodynamics_calcs.m
@@ -131,23 +131,37 @@ h_r3=h_iron+h_water-(h_wustite+h_hydrogen); % happens only if T>570
 
 h_r4=3*h_iron+4*h_water-(h_magnetite+4*h_hydrogen); % happens only if T<570
 
-
+%s's are in J/mol*K not kJ!!!!
 s_r1= 2*s_magnetite + s_water - (3* s_hematite + s_hydrogen);
 s_r2= 3*s_wustite+s_water-(s_magnetite+s_hydrogen); % happens only if T>570
 s_r3=s_iron+h_water-(s_wustite+s_hydrogen); % happens only if T>570
 
 s_r4=3*s_iron+4*s_water-(s_magnetite+4*s_hydrogen); % happens only if T<570
 
+g_r1= h_r1 - T.*s_r1;
+g_r2= h_r2 - T.*s_r2;
+g_r3= h_r3 - T.*s_r3;
+
+g_r4= h_r4 - T.*s_r4;
+
 %the overall reactions. starting with 1 mol Fe2O3.
 for i=1:length(T)
     if T(i)>570
-        h_r(i)= (1/3)*h_r1(i) + (2/3)*h_r2(i) + 2*h_r3(i);
-        s_r(i)= (1/3)*s_r1(i) + (2/3)*s_r2(i) + 2*s_r3(i);
-        g_r(i)= h_r(i) - T(i)*s_r(i);
+        h_r(i)= (1/3)*h_r1(i) + (2/3)*h_r2(i) + 2*h_r3(i); %kJ/mol
+        s_r(i)= (1/3)*s_r1(i) + (2/3)*s_r2(i) + 2*s_r3(i); 
+        g_r(i)= h_r(i) - T(i)*s_r(i)/1000; %entropy is originally in J/mol*K (need to divide entropy by 1000)
+
+        %{
+        %g_r should be the same as g2_r:
+        g2_r(i) = (1/3)*g_r1(i) + (2/3)*g_r2(i) + 2*g_r3(i);
+        %I checked and g2_r does indeed give the same result as g_r. as
+        expected.
+        %}
     else
         disp('Error. for T<570, the overall reaction is I --> IV.  Need to write expression for this.')
     end
 end
+
 
 % if moisture goes 60% how will it vary
 %% Plot
@@ -163,7 +177,7 @@ title('Enthalpies of Reaction for HyDR')
 legend(['\DeltaH_R I'],['\DeltaH_R II'],['\DeltaH_R III'],['\DeltaH_R IV'],['\DeltaH_R overall'],'Location','eastoutside')
 xlim([min(T) max(T)])
 box on
-set(gca,'FontSize',15)
+set(gca,'FontSize',12)
 f=gcf;
 f.Position = [93.8000 439.4000 638.4000 336.8000];
 
@@ -174,26 +188,101 @@ plot(T,s_r3,'--k','LineWidth',2); hold on
 plot(T,s_r4,'--m','Linewidth',2); hold on
 plot(T,s_r,'-b','LineWidth',2); 
 xlabel('Temperature (K)','interpreter','latex')
-ylabel('Entropy of reaction (kJ/mol)','interpreter','latex')
+ylabel('Entropy of reaction (J/mol)','interpreter','latex')
 title('Entropies of Reaction for HyDR')
 legend(['\DeltaS_R I'],['\DeltaS_R II'],['\DeltaS_R III'],['\DeltaS_R IV'],['\DeltaS_R overall'],'Location','eastoutside')
 xlim([min(T) max(T)])
 box on
-set(gca,'FontSize',15)
+set(gca,'FontSize',12)
 g=gcf;
 g.Position = [737.8000 439.4000 639.2000 336.8000];
 
 figure
+plot(T,g_r1,'--r', 'LineWidth',2); hold on
+plot(T,g_r2,'--g','LineWidth',2); hold on
+plot(T,g_r3,'--k','LineWidth',2); hold on
+plot(T,g_r4,'--m','Linewidth',2); hold on
 plot(T,g_r,'-b','LineWidth',2); 
 xlabel('Temperature (K)','interpreter','latex')
 ylabel('Gibbs of reaction (kJ/mol)','interpreter','latex')
 title('Gibbs of Reaction for HyDR')
-legend(['\DeltaG_R overall'],'Location','eastoutside')
+legend(['\DeltaG_R I'],['\DeltaG_R II'],['\DeltaG_R III'],['\DeltaG_R IV'],['\DeltaG_R overall'],'Location','eastoutside')
 xlim([min(T) max(T)])
 box on
-
-set(gca,'FontSize',15)
+set(gca,'FontSize',12)
 m=gcf;
 m.Position = [426.6000 50.6000 689.6000 308.8000];
 
-figure; plot(T,s_hematite); hold on; plot(T,s_magnetite); hold on; plot(T,s_wustite); legend('hem','mag','wus')
+figure; tiledlayout(1,2);
+nexttile;
+plot(T,h_hematite,'Linewidth',2); hold on; plot(T,h_magnetite,'Linewidth',2); hold on; plot(T,h_wustite,'Linewidth',2); plot(T,h_water,'Linewidth',2); plot(T,h_hydrogen,'Linewidth',2); 
+title('Enthalpies of species')
+xlabel('Temperature (K)','interpreter','latex')
+ylabel('Enthalpy of formation (kJ/mol)','interpreter','latex')
+xlim([min(T) max(T)])
+set(gca,'FontSize',12)
+
+nexttile; plot(T,s_hematite,'Linewidth',2); hold on; plot(T,s_magnetite,'Linewidth',2); hold on; plot(T,s_wustite,'Linewidth',2); plot(T,s_water,'Linewidth',2); plot(T,s_hydrogen,'Linewidth',2); 
+legend('Fe_2O_3','Fe_3O_4','FeO','H_2O','H_2','Location','eastoutside')
+title('Entropies of species')
+xlabel('Temperature (K)','interpreter','latex')
+ylabel('Entropy of formation (J/mol*K)','interpreter','latex')
+xlim([min(T) max(T)])
+set(gca,'FontSize',12)
+
+%% Approximation, assuming the heats and entropies of rxn are T invariant. 
+
+% using the standard enthalpies and entropies of the species, from NIST
+% ("standard" superscript means 25C and 1atm unless otherwise noted):
+h_hematite_std = -825.5032; %kJ/mol
+s_hematite_std = 87.28/1000; %kJ/mol*K
+
+h_magnetite_std = -1120.894;
+s_magnetite_std = 147.06/1000; %not given by NIST. I used the standard entropy from 
+% https://deepblue.lib.umich.edu/bitstream/handle/2027.42/33058/0000444.pdf?sequence=1
+% instead. @300K: 35.147 cal/mol*K --> 147.06 J/mol*K
+
+h_wustite_std = -272.04	; %solid
+s_wustite_std = 60.75/1000; %solid
+
+h_iron_std = 0;
+s_iron_std = 27.31/1000; %@298K: 27.31 J/mol*K
+
+h_water_std = -241.83; %gas
+s_water_std = 188.84/1000; %gas, 1 bar
+
+h_hydrogen_std = 0;
+s_hydrogen_std = 130.68/1000; %gas, 1 bar
+
+% Calculating the approximate energies of the reactions (kJ/mol or kJ/mol*K)
+h_std_rxn1 = 2*h_magnetite_std + h_water_std - (3*h_hematite_std + h_hydrogen_std)
+s_std_rxn1 = 2*s_magnetite_std + s_water_std - (3*s_hematite_std + s_hydrogen_std)
+
+h_std_rxn2 = 3*h_wustite_std + h_water_std - (h_magnetite_std + h_hydrogen_std)
+s_std_rxn2 = 3*s_wustite_std + s_water_std - (s_magnetite_std + s_hydrogen_std)
+
+h_std_rxn3 = h_iron_std + h_water_std - (h_wustite_std + h_hydrogen_std)
+s_std_rxn3 = s_iron_std + s_water_std - (s_wustite_std + s_hydrogen_std)
+
+g_std_rxn1 = h_std_rxn1 - T.*s_std_rxn1
+g_std_rxn2 = h_std_rxn2 - T.*s_std_rxn2
+g_std_rxn3 = h_std_rxn3 - T.*s_std_rxn3
+
+g_std_overall_1_3 = g_std_rxn1 + 2*g_std_rxn2 + 6*g_std_rxn3 %NOTE: this requires 3 mol of Fe2O3 for the 
+% rxn to initiate, so I'm NOT dividing the overall expression by 3.
+
+%}
+figure
+plot(T,g_std_rxn1,'--r', 'LineWidth',2); hold on
+plot(T,g_std_rxn2,'--g','LineWidth',2); hold on
+plot(T,g_std_rxn3,'--k','LineWidth',2); hold on
+plot(T,g_std_overall_1_3,'-b','LineWidth',2); hold on
+xlabel('Temperature (K)','interpreter','latex'); ylabel('Gibbs of reaction (kJ/mol)','interpreter','latex')
+title('Approximated Gibbs of Reaction for HyDR')
+legend(['\DeltaG_R I: 3Fe_2O3 + H_2 \rightarrow 2Fe_3O_4 + H_2O'], ...
+    ['\DeltaG_R II: Fe_3O_4 + H_2 \rightarrow 3FeO + H_2O'], ...
+    ['\DeltaG_R III: FeO + H_2 \rightarrow Fe + 6H_2O'], ...
+    ['\DeltaG_R overall: 3Fe_2O_3 + H_2 \rightarrow 6Fe + 6H_2O'],'Location','southoutside')
+xlim([min(T) max(T)])
+box on
+set(gca,'FontSize',12)
